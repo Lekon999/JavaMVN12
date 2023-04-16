a1885f8f081b5fa1c56cb270c33525cf4762e5f2
Кочнев Илья <Bankir369@gmail.com>
2023-04-17 00:29:36 +0300

    Inicial commit

--- src/main/java/org/example/FilmsManager.java
+++ src/main/java/org/example/FilmsManager.java
@@ -2,20 +2,19 @@ package org.example;
 
 public class FilmsManager {
     private String[] films = new String[0];
-    private int limit;
+    private final int limit;
 
     public FilmsManager() {
         this.limit = 5;
     }
+
     public FilmsManager(int limit) {
         this.limit = limit;
     }
 
     public void addFilm(String film) {
-        String[] tmp = new  String[films.length + 1];
-        for (int i = 0; i < films.length; i++) {
-            tmp[i] = films[i];
-        }
+        String[] tmp = new String[films.length + 1];
+        System.arraycopy(films, 0, tmp, 0, films.length);
         tmp[tmp.length - 1] = film;
         films = tmp;
 
@@ -27,19 +26,11 @@ public class FilmsManager {
     }
 
     public String[] findLast() {
-        int resultLenght;
-        if (films.length < limit) {
-            resultLenght = films.length;
-        } else {
-            resultLenght = limit;
-        }
+        int resultLenght = Math.min(films.length, limit);
         String[] tmp = new String[resultLenght];
         for (int i = 0; i < tmp.length; i++) {
             tmp[i] = films[films.length - 1 - i];
         }
         return tmp;
-
-
     }
-
 }
